8585f330f58304a54ee6308b5ff08b4666d4043b
Jesus Ponce <chuyp11@gmail.com>
2018-03-28 16:17:22 -0600

    28/03/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { ProveedoresService } from './servicios/proveedores.service';
+import { PresupuestosService } from './servicios/presupuestos.service';
 
 import { AppComponent } from './app.component';
 import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { HeaderComponent } from './header/header.component';
 import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+import { AddpresComponent } from './presupuestos/addpres/addpres.component';
+import { PresupuestosComponent } from './presupuestos/presupuestos/presupuestos.component';
+import { EditpresComponent } from './presupuestos/editpres/editpres.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'proveedores', component: ProveedoresComponent },
   { path: 'addprovee', component:AddproveeComponent},
-  { path: '**', component: InicioComponent},
+  { path: 'addpres', component: AddpresComponent},
+  { path: 'presupuestos', component: PresupuestosComponent },
+  { path: 'editpres/:id', component: EditpresComponent },
+  { path: '**', component: InicioComponent}
 ];
 
 @NgModule({
@@ -24,14 +34,19 @@ const routes: Routes = [
     ProveedoresComponent,
     InicioComponent,
     HeaderComponent,
-    AddproveeComponent
+    AddproveeComponent,
+    AddpresComponent,
+    PresupuestosComponent,
+    EditpresComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [ProveedoresService],
+  providers: [ProveedoresService,PresupuestosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/proveedores/addprovee/addprovee.component.ts
+++ src/app/proveedores/addprovee/addprovee.component.ts
@@ -9,7 +9,18 @@ import { NgForm } from '@angular/forms';
 export class AddproveeComponent implements OnInit {
 
   @ViewChild('formpro') formpro: NgForm;
+
   proveedor: any;
+
+  provincias: string[] = [
+    'Álava','Albacete','Alicante','Almería','Asturias','Ávila','Badajoz','Barcelona',
+    'Burgos', 'Cáceres', 'Cádiz','Cantabria','Castellón','Ciudad Real','Córdoba',
+    'La Coruña','Cuenca','Gerona','Granada','Guadalajara',
+    'Guipúzcoa','Huelva','Huesca','IslasBaleares','Jaén','León','Lérida','Lugo',
+    'Madrid', 'Málaga','Murcia','Navarra','Orense','Palencia','Las Palmas',
+    'Pontevedra','La Rioja','Salamanca','Segovia','Sevilla','Soria','Tarragona',
+    'Santa Cruz de Tenerife', 'Teruel', 'Toledo', 'Valencia', 'Valladolid', 'Vizcaya',
+    'Zamora','Zaragoza' ]
 
   constructor() {
     this.proveedor = {
